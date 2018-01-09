ca8627dd6b5d1fa91aed7c1a0d35fb2a7f4eeeab
Denise <denise.klep@student.ru.nl>
2018-01-09 20:13:40 +0100

    1.2 doet het

--- SML4.m
+++ SML4.m
@@ -5,3 +5,32 @@ for i=1:5
    x = x_prev + (1/sin(x_prev)) * cos(x_prev)
    x_prev = x; 
 end
+
+
+phi = [[1,0.3];
+       [1,0.44];
+       [1,0.46];
+       [1,0.6]];
+% y = phi(:,2)'
+t = [1,0,1,0];
+w_prev = [1.0,1.0]';
+R = diag(y.*(1.-y));
+for i=1:5
+%     y.*(1-y)
+%     w_prev.*w_prev
+%     (phi*w_prev')
+%     inv(R)
+%     (y-t)
+%     z = (phi*w_prev)-(inv(R))*(y-t)';
+%     w = inv(phi'*R*phi)*phi'*R*z
+%     -(phi*w_prev)
+%     exp(  -(phi*w_prev))
+%     1+(exp(  -(phi*w_prev)))
+%     1/(1+(exp(  -(phi*w_prev))))
+    y = 1./(1+exp(-(phi*w_prev)));
+    R = diag(y.*(1.-y));
+    z = (phi*w_prev)-(inv(R))*(y'-t)';
+    w = inv(phi'*R*phi)*phi'*R*z
+%     w = w_prev - inv(phi'*R*phi)*phi'*(y'-t)'
+    w_prev = w;
+end
